d08025ee1d91cb6906b22559ed88848890e4140d
sajal48 <newtonh48@gmail.com>
2023-02-13 21:13:38 +0600

    hibernate security (error)

--- src/main/java/digitech/service/UserService.java
+++ src/main/java/digitech/service/UserService.java
@@ -6,12 +6,18 @@ import digitech.model.Role;
 import digitech.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class UserService  {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserDao userDao;
@@ -43,6 +49,24 @@ public class UserService  {
         return userDao.findByEmail(email);
 
     }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userDao.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Username not found");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+                getGrantedAuthorities(user));
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(User user) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority(user.getRole().getRoleName()));
+
+        return authorities;
+}
 
 
 }
